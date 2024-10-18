375611d66edb9d1ab07d08499184fb4d19f275f5
George Leyva <leyvageorge@hotmail.com>
2024-10-18 16:33:03 -0700

    added levels and score

--- scene/level/level.gd
+++ scene/level/level.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 const ANIMAL = preload("res://scene/animal/animal.tscn")
+const MAIN = preload("res://scene/main/main.tscn")
+
 #Location of where the bird will start
 @onready var animal_start: Marker2D = $AnimalStart
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_animal()
@@ -11,7 +12,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_key_pressed(KEY_ESCAPE): #changes the scene back to main menu when escape is pressed
+		get_tree().change_scene_to_packed(MAIN) 
 
 #creates an instance and moves it to position
 func add_animal() -> void:
